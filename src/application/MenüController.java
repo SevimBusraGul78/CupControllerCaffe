31f2428962fb350a6dc9c0ac765d8f4106e1a1fb
Sevim Büşra <sevimbusra78@gmail>
2024-05-10 22:11:39 +0300

    fiş eklendi

--- src/application/MenüController.java
+++ src/application/MenüController.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,12 +11,17 @@ import com.IsteMYSql.Util.VeritabaniUtil;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MenüController {
+
     public MenüController() {
         baglanti = VeritabaniUtil.Baglan();
     }
@@ -46,6 +52,7 @@ public class MenüController {
 
     @FXML
     private Text txtSonuc;
+
     @FXML
     private TextField kullaniciadi;
 
@@ -55,9 +62,11 @@ public class MenüController {
     @FXML
     void btn1(ActionEvent event) {
         sepeteEkle(50, "kahve");
+        
     }
 
-    @FXML
+   
+	@FXML
     void btn2(ActionEvent event) {
         sepeteEkle(30, "kek");
     }
@@ -75,21 +84,35 @@ public class MenüController {
     private void sepeteEkle(double fiyat, String urun) {
         toplamFiyat += fiyat;
         txtSonuc.setText(" " + toplamFiyat);
-        urunler += urun + ",";
+        urunler += urun + "\n\n\n\n";
         txtürün.setText(urunler);
     }
-   
+
     @FXML
     void btnsiparisVer(ActionEvent event) {
-        String sql = "INSERT INTO islemler (user,islemAcıklama, islemtutar) VALUES (?, ?,?)";
+        String sql = "INSERT INTO islemler (user, islemAcıklama, islemtutar) VALUES (?, ?, ?)";
         try {
             sorguİfadesi = baglanti.prepareStatement(sql);
             sorguİfadesi.setString(1, kullaniciadi.getText().trim());
             sorguİfadesi.setString(2, txtürün.getText().trim());
             sorguİfadesi.setDouble(3, Double.parseDouble(txtSonuc.getText().trim()));
-           
+
             sorguİfadesi.executeUpdate();
-        } catch (SQLException e) {
+
+            // Yeni bir pencere aç
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("fiş.fxml")); // YeniSayfa.fxml'yi yükleyin
+            Parent root = loader.load(); // Yüklenen FXML'i bir ebeveyn düğüm olarak yükleyin
+            Stage stage = new Stage(); // Yeni bir sahne oluşturun
+            stage.setScene(new Scene(root)); // Sahneye kökü ayarlayın
+            stage.show(); // Pencereyi göster
+            
+            // txtisim TextField'ının değerini alarak yeni açılan pencereye aktarım işlemi
+            fişController fişController = loader.getController();
+            fişController.getTxtisim().setText(kullaniciadi.getText().trim());
+           
+           fişController.getÜrün2().setText(urunler);
+
+        } catch (SQLException | IOException e) {
             e.printStackTrace(); // Hata mesajını yazdır
         }
     }
@@ -101,4 +124,8 @@ public class MenüController {
 
     private double toplamFiyat = 0.0;
     private String urunler = "";
+
+    // TextFieldDegeriAlmaSınıfı iç içe sınıf olarak değil, ayrı bir dosyada bulunabilir.
+    // Ancak iç içe sınıf olarak kullanılacaksa MenüController sınıfının içinde değil, dışında tanımlanmalıdır.
+   
 }
---
+++ src/application/fişController.java
@@ -0,0 +1,145 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.MenüController;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class fişController  {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+	private Text txtisim;
+    @FXML
+    private Text fiyat1;
+
+    @FXML
+    private Text fiyat2;
+
+    @FXML
+    private Text fiyat3;
+
+    @FXML
+    private Text fiyat4;
+
+    @FXML
+    private ImageView img1;
+
+    @FXML
+    private ImageView img2;
+
+    @FXML
+    private Text toplam;
+
+    @FXML
+    private Text toplamfiyat;
+
+    @FXML
+    private Text ürün1;
+
+    public Text getFiyat1() {
+		return fiyat1;
+	}
+
+	public void setFiyat1(Text fiyat1) {
+		this.fiyat1 = fiyat1;
+	}
+
+	public Text getFiyat2() {
+		return fiyat2;
+	}
+
+	public void setFiyat2(Text fiyat2) {
+		this.fiyat2 = fiyat2;
+	}
+
+	public Text getFiyat3() {
+		return fiyat3;
+	}
+
+	public void setFiyat3(Text fiyat3) {
+		this.fiyat3 = fiyat3;
+	}
+
+	public Text getFiyat4() {
+		return fiyat4;
+	}
+
+	public void setFiyat4(Text fiyat4) {
+		this.fiyat4 = fiyat4;
+	}
+
+	public Text getÜrün1() {
+		return ürün1;
+	}
+
+	public void setÜrün1(Text ürün1) {
+		this.ürün1 = ürün1;
+	}
+
+	public Text getÜrün2() {
+		return ürün2;
+	}
+
+	public void setÜrün2(Text ürün2) {
+		this.ürün2 = ürün2;
+	}
+
+	public Text getÜrün3() {
+		return ürün3;
+	}
+
+	public void setÜrün3(Text ürün3) {
+		this.ürün3 = ürün3;
+	}
+
+	public Text getÜrün4() {
+		return ürün4;
+	}
+
+	public void setÜrün4(Text ürün4) {
+		this.ürün4 = ürün4;
+	}
+
+	@FXML
+    private Text ürün2;
+
+    @FXML
+    private Text ürün3;
+
+    @FXML
+    private Text ürün4;
+
+    public class TextFieldDegeriAlmaSınıfı {
+        public void textFieldDegeriniAl(String deger) {
+            Text text = new Text();
+			// FişController sınıfındaki txtisim TextField'ına değişken değerini aktarır
+            text.setText(deger);
+        }
+    }
+   
+    @FXML
+    void initialize() {
+    
+    	}
+
+	public Text getTxtisim() {
+		return txtisim;
+	}
+
+	public void setTxtisim(Text txtisim) {
+		this.txtisim = txtisim;
+	}
+
+
+    }
+
+
